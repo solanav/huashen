bf1ff5df94acead485b7f33894576247362527a8
solanav <solanav@qq.com>
2019-11-03 15:53:36 +0100

    Added main loop to keep receiving commands

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,6 @@ mod os_utils;
 mod net_utils;
 
 fn main() {
-    net_utils::start_server()
+    net_utils::start_server("127.0.0.1", "8080")
         .expect("Failed to start the server");
 }
--- src/net_utils.rs
+++ src/net_utils.rs
@@ -14,17 +14,22 @@ fn send_output(other: OtherInfo, s: String) -> Result<(), Box<dyn Error>> {
 }
 
 fn handle_msg(other: OtherInfo, msg: &[u8], num_bytes: usize) -> Result<(), Box<dyn Error>> {
+    // Get only the first num_bytes characters    
     let mut s = Vec::new();
     for c in 0..num_bytes {
         s.push(msg[c]);
     }
-        
+    
+    // Turn array to string
     let mut command = String::new();
     match str::from_utf8(&s) {
         Ok(com) => command = com.to_string(),
         Err(_) => (),
     };
 
+    println!("Received [{}] from [{}]", command, other.addr.ip().to_string());
+
+    // Execute command
     let out = match os_utils::execute_command(&command) {
         Ok(output) => output.to_string(),
         Err(_) => String::from("Invalid command, no result"),
@@ -36,14 +41,16 @@ fn handle_msg(other: OtherInfo, msg: &[u8], num_bytes: usize) -> Result<(), Box<
     Ok(())
 }
 
-pub fn start_server() -> Result<(), Box<dyn Error>> {
-    let socket = UdpSocket::bind("127.0.0.1:8082")?;
+pub fn start_server(ip: &str, port: &str) -> Result<(), Box<dyn Error>> {
+    println!("Listening on {}:{}", ip, port);
 
-    let mut buf = [0; 128];
-    let (num_bytes, src_addr) = socket.recv_from(&mut buf)?;
+    loop {
+        let socket = UdpSocket::bind(format!("{}:{}", ip, port))?;
 
-    let other = OtherInfo {socket: &socket, addr: src_addr};
-    handle_msg(other, &buf, num_bytes)?;
+        let mut buf = [0; 128];
+        let (num_bytes, src_addr) = socket.recv_from(&mut buf)?;
 
-    Ok(())
+        let other = OtherInfo {socket: &socket, addr: src_addr};
+        handle_msg(other, &buf, num_bytes)?;
+    }
 }
