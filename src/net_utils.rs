7d345025351e4ea8b7952af332e1427e901ddfd8
solanav <solanav@qq.com>
2019-11-03 12:35:19 +0100

    Added remote shell execution

--- src/net_utils.rs
+++ src/net_utils.rs
@@ -1,31 +1,49 @@
 use std::net::UdpSocket;
-use std::io::Result;
 use std::str;
+use std::error::Error;
 use super::os_utils;
 
-fn handle_msg(num_bytes: usize, src_addr: std::net::SocketAddr, msg: &[u8]) {
+struct OtherInfo<'a> {
+    socket: &'a UdpSocket,
+    addr: std::net::SocketAddr,
+}
+
+fn send_output(other: OtherInfo, s: String) -> Result<(), Box<dyn Error>> {
+    other.socket.send_to(s.as_bytes(), other.addr)?;
+    Ok(())
+}
+
+fn handle_msg(other: OtherInfo, msg: &[u8], num_bytes: usize) -> Result<(), Box<dyn Error>> {
     let mut s = Vec::new();
     for c in 0..num_bytes {
         s.push(msg[c]);
     }
+        
+    let mut command = String::new();
+    match str::from_utf8(&s) {
+        Ok(com) => command = com.to_string(),
+        Err(_) => (),
+    };
 
-    println!("{}", str::from_utf8(&s)
-        .expect("Failed to parse utf-8")
-        .to_string());
+    let out = match os_utils::execute_command(&command) {
+        Ok(output) => output.to_string(),
+        Err(_) => String::from("Invalid command, no result"),
+    };
+    
+    // Send output to server
+    send_output(other, out)?;
 
-    let out = os_utils::execute_command(str::from_utf8(&s)
-        .expect("Failed to parse utf-8")
-        .to_string());
-
-    println!("Output >\n{}", out);
+    Ok(())
 }
 
-pub fn start_server() -> Result<()> {
+pub fn start_server() -> Result<(), Box<dyn Error>> {
     let socket = UdpSocket::bind("127.0.0.1:8082")?;
 
     let mut buf = [0; 128];
     let (num_bytes, src_addr) = socket.recv_from(&mut buf)?;
-    handle_msg(num_bytes, src_addr, &buf);
+
+    let other = OtherInfo {socket: &socket, addr: src_addr};
+    handle_msg(other, &buf, num_bytes)?;
 
     Ok(())
 }
--- src/os_utils.rs
+++ src/os_utils.rs
@@ -1,26 +1,24 @@
-use std::process;
+use std::process::Command;
 use std::str;
+use std::io;
 
-pub fn execute_command(s: String) -> String {
+pub fn execute_command(s: &String) -> Result<String, io::Error> {
     // Split the command on spaces
     let command_list: Vec<&str> = s.split(" ").collect();
     
     // Add the program to run
-    let mut command = process::Command::new(command_list[0]);
+    let mut command = Command::new(command_list[0]);
 
     // Add the arguments
-    if command_list.len() > 1 {
-        for arg in command_list {
-            command.arg(arg);
-        }
+    for arg in command_list {
+        command.arg(arg);
     }
 
     // Execute the command
-    let output = command.output()
-        .expect("Failed to execute command");
+    let output = command.output()?;
 
     // Return a string with output
-    str::from_utf8(&output.stdout)
+    Ok(str::from_utf8(&output.stdout)
         .expect("Failed to parse char")
-        .to_string()
+        .to_string())
 }
